953ff6587a2030cdd4998542dafcc75ed5e38fff
Antonette_Ortiz <ORTIZANTONETTE@GMAIL.COM>
2020-12-09 23:03:16 -0500

    updated routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-import HomePage from './components/HomePage'
+import LandingPage from './components/LandingPage';
+import HomePage from './components/HomePage';
+import Language from './components/Language';
+import Recipe from './components/Recipe'
 import Music from './components/Music';
+import Forum from './components/Forum'
 
 
 
 export default (
-    <Route path="/music" component={Music} />
+  <Switch>
+    <Route path="/welcome" component={LandingPage} />
     <Route path="/home" component={HomePage} />
+    <Route path="/language" component={Language} />
+    <Route path="/recipe" component={Recipe} />
+    <Route path="/music" component={Music} />
+    <Route path="/forum" component={Forum} />
+  </Switch>
 );
