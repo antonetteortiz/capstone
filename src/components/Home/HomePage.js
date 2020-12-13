849eb11391750b75d11be9961ed03b71b2136ef8
Antonette_Ortiz <ORTIZANTONETTE@GMAIL.COM>
2020-12-13 11:51:10 -0500

    updated feature containers on homepage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,19 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./HomePage.css";
 import Navbar from "../Navbar/Navbar";
-import { Carousel } from "react-bootstrap";
 import Slide2 from "/Users/antonetteortiz/Desktop/sei40/projects/capstone/src/images/garifunaboys.jpg";
 import Slide3 from "/Users/antonetteortiz/Desktop/sei40/projects/capstone/src/images/hudut.jpg";
-
 import Slide6 from "/Users/antonetteortiz/Desktop/sei40/projects/capstone/src/images/soup.jpg";
-
 import History from "/Users/antonetteortiz/Desktop/sei40/projects/capstone/src/images/history.png";
 import Food from "/Users/antonetteortiz/Desktop/sei40/projects/capstone/src/images/food.jpg";
 import Language from "/Users/antonetteortiz/Desktop/sei40/projects/capstone/src/images/yurumeilanguage.jpg";
 import Music from "/Users/antonetteortiz/Desktop/sei40/projects/capstone/src/images/music.jpg";
 import Forum from "/Users/antonetteortiz/Desktop/sei40/projects/capstone/src/images/forum.png";
-import Footer from "../Footer/Footer";
+
 
 function HomePage() {
   return (
@@ -37,6 +33,7 @@ function HomePage() {
               style={{ maxWidth: "100%", maxHeight: "565px" }}
               src={Slide2}
               preserveAspectRatio="xMidYMid slice"
+              alt=""
               // focusable="false"
               // role="img"
             />
@@ -48,6 +45,7 @@ function HomePage() {
               style={{ maxWidth: "100%", height: "565px" }}
               src={Slide3}
               preserveAspectRatio="xMidYMid slice"
+              alt=""
             />
           </div>
 
@@ -57,6 +55,7 @@ function HomePage() {
               style={{ maxWidth: "100%", maxHeight: "565px" }}
               src={Slide6}
               preserveAspectRatio="xMidYMid slice"
+              alt=""
             />
           </div>
         </div>
@@ -105,22 +104,13 @@ function HomePage() {
           </p>
         </div>
         <div class="col-md-5">
-          <svg
-            class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-            width="500"
-            height="500"
-            xmlns="http://www.w3.org/2000/svg"
+          <img
+            classNameName="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            style={{ width: "700px", height: "500px" }}
+            src={History}
             preserveAspectRatio="xMidYMid slice"
-            focusable="false"
-            role="img"
-            aria-label="Placeholder: 500x500"
-          >
-            <title>Placeholder</title>
-            <rect width="100%" height="100%" fill="#eee" />
-            <text x="50%" y="50%" fill="#aaa" dy=".3em">
-              500x500
-            </text>
-          </svg>
+            alt=""
+          />
         </div>
       </div>
 
@@ -140,22 +130,13 @@ function HomePage() {
           </p>
         </div>
         <div class="col-md-5 order-md-1">
-          <svg
-            class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-            width="500"
-            height="500"
-            xmlns="http://www.w3.org/2000/svg"
+          <img
+            classNameName="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            style={{ width: "700px", height: "500px" }}
+            src={Language}
             preserveAspectRatio="xMidYMid slice"
-            focusable="false"
-            role="img"
-            aria-label="Placeholder: 500x500"
-          >
-            <title>Placeholder</title>
-            <rect width="100%" height="100%" fill="#eee" />
-            <text x="50%" y="50%" fill="#aaa" dy=".3em">
-              500x500
-            </text>
-          </svg>
+            alt=""
+          />
         </div>
       </div>
 
@@ -174,22 +155,64 @@ function HomePage() {
           </p>
         </div>
         <div class="col-md-5">
-          <svg
-            class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-            width="500"
-            height="500"
-            xmlns="http://www.w3.org/2000/svg"
+          <img
+            classNameName="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            style={{ width: "700px", height: "500px" }}
+            src={Food}
             preserveAspectRatio="xMidYMid slice"
-            focusable="false"
-            role="img"
-            aria-label="Placeholder: 500x500"
-          >
-            <title>Placeholder</title>
-            <rect width="100%" height="100%" fill="#eee" />
-            <text x="50%" y="50%" fill="#aaa" dy=".3em">
-              500x500
-            </text>
-          </svg>
+            alt=""
+          />
+        </div>
+      </div>
+
+      <hr class="featurette-divider" />
+
+      <div class="row featurette">
+        <div class="col-md-7 order-md-2">
+          <h2 class="featurette-heading">
+            Oh yeah, it’s that good.{" "}
+            <span class="text-muted">See for yourself.</span>
+          </h2>
+          <p class="lead">
+            Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id
+            ligula porta felis euismod semper. Praesent commodo cursus magna,
+            vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
+            commodo.
+          </p>
+        </div>
+        <div class="col-md-5 order-md-1">
+          <img
+            classNameName="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            style={{ width: "700px", height: "500px" }}
+            src={Music}
+            preserveAspectRatio="xMidYMid slice"
+            alt=""
+          />
+        </div>
+      </div>
+
+      <hr class="featurette-divider" />
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">
+            And lastly, this one. <span class="text-muted">Checkmate.</span>
+          </h2>
+          <p class="lead">
+            Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id
+            ligula porta felis euismod semper. Praesent commodo cursus magna,
+            vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
+            commodo.
+          </p>
+        </div>
+        <div class="col-md-5">
+          <img
+            classNameName="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            style={{ width: "700px", height: "500px" }}
+            src={Forum}
+            preserveAspectRatio="xMidYMid slice"
+            alt=""
+          />
         </div>
       </div>
 
--- src/components/Language/Language.js
+++ src/components/Language/Language.js
@@ -5,6 +5,132 @@ function Language() {
   return (
     <div>
       <Navbar />
+      <hr classNameName="featurette-divider" />
+
+      <div classNameName="row featurette">
+        <div classNameName="col-md-7">
+          <h2 classNameName="featurette-heading">
+            First featurette heading.{" "}
+            <span classNameName="text-muted">It’ll blow your mind.</span>
+          </h2>
+          <p classNameName="lead">
+            Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id
+            ligula porta felis euismod semper. Praesent commodo cursus magna,
+            vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
+            commodo.
+          </p>
+        </div>
+        <div classNameName="col-md-5">
+          <img
+            classNameName="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            style={{ width: "700px", height: "500px" }}
+            src={History}
+            preserveAspectRatio="xMidYMid slice"
+          />
+        </div>
+      </div>
+
+      <hr classNameName="featurette-divider" />
+
+      <div classNameName="row featurette">
+        <div classNameName="col-md-7 order-md-2">
+          <h2 classNameName="featurette-heading">
+            Oh yeah, it’s that good.{" "}
+            <span classNameName="text-muted">See for yourself.</span>
+          </h2>
+          <p classNameName="lead">
+            Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id
+            ligula porta felis euismod semper. Praesent commodo cursus magna,
+            vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
+            commodo.
+          </p>
+        </div>
+        <div classNameName="col-md-5 order-md-1">
+          <img
+            classNameName="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            style={{ width: "700px", height: "500px" }}
+            src={Language}
+            preserveAspectRatio="xMidYMid slice"
+          />
+        </div>
+      </div>
+
+      <hr classNameName="featurette-divider" />
+
+      <div classNameName="row featurette">
+        <div classNameName="col-md-7">
+          <h2 classNameName="featurette-heading">
+            First featurette heading.{" "}
+            <span classNameName="text-muted">It’ll blow your mind.</span>
+          </h2>
+          <p classNameName="lead">
+            Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id
+            ligula porta felis euismod semper. Praesent commodo cursus magna,
+            vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
+            commodo.
+          </p>
+        </div>
+        <div classNameName="col-md-5">
+          <img
+            classNameName="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            style={{ width: "700px", height: "500px" }}
+            src={Food}
+            preserveAspectRatio="xMidYMid slice"
+          />
+        </div>
+      </div>
+
+      <hr classNameName="featurette-divider" />
+
+      <div classNameName="row featurette">
+        <div classNameName="col-md-7 order-md-2">
+          <h2 classNameName="featurette-heading">
+            Oh yeah, it’s that good.{" "}
+            <span classNameName="text-muted">See for yourself.</span>
+          </h2>
+          <p classNameName="lead">
+            Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id
+            ligula porta felis euismod semper. Praesent commodo cursus magna,
+            vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
+            commodo.
+          </p>
+        </div>
+        <div classNameName="col-md-5 order-md-1">
+          <img
+            classNameName="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            style={{ width: "700px", height: "500px" }}
+            src={Music}
+            preserveAspectRatio="xMidYMid slice"
+          ></img>
+        </div>
+      </div>
+
+      <hr classNameName="featurette-divider" />
+
+      <div classNameName="row featurette">
+        <div classNameName="col-md-7">
+          <h2 classNameName="featurette-heading">
+            First featurette heading.{" "}
+            <span classNameName="text-muted">It’ll blow your mind.</span>
+          </h2>
+          <p classNameName="lead">
+            Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id
+            ligula porta felis euismod semper. Praesent commodo cursus magna,
+            vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
+            commodo.
+          </p>
+        </div>
+        <div classNameName="col-md-5">
+          <img
+            classNameName="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            style={{ width: "700px", height: "500px" }}
+            src={Forum}
+            preserveAspectRatio="xMidYMid slice"
+          ></img>
+        </div>
+      </div>
+
+      <hr classNameName="featurette-divider" />
     </div>
   );
 }
